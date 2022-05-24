657d37673f2ef9443672cfc10cad2d4690504dd9
coden-arfat <52942762+coden-arfat@users.noreply.github.com>
2022-05-24 18:41:39 +0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+export default function Home() {
+  return (
+
+    <>
+    <h1>
+      hello world
+      
+    </h1>
+    </>
+
+  )
+}
